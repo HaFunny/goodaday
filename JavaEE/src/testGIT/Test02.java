0706fe3c93193a4eb368b29df4200597dc39f7bb
HaFunny <390473616@qq.com>
2020-09-03 22:48:58 +0800

    重新测试gitee&github同时提交

--- JavaEE/src/testGIT/Test02.java
+++ JavaEE/src/testGIT/Test02.java
@@ -3,5 +3,8 @@ package testGIT;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("ruserious");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
